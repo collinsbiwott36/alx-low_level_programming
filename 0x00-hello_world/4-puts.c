e518c4e9b8ef4c67fcc1837a7bc02fa213f2bbad
collinsbiwott36 <collinsbiwott36@gmail.com>
2022-06-02 13:19:47 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * /
+
+int main(void)
+
+{
+
+		char str[] = "\"Programming is like building a multilingual puzzle";
+
+		puts(str);
+		
+		return (0);
+
+}

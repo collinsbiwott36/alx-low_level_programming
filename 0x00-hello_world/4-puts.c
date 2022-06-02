6807b2b634e2bd26d07b594a395a1c7dacd0cae0
collinsbiwott36 <collinsbiwott36@gmail.com>
2022-06-02 13:32:00 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,21 +2,16 @@
 #include <string.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
-
 {
-
 		char str[] = "\"Programming is like building a multilingual puzzle";
 
 		puts(str);
 		
 		return (0);
-
 }

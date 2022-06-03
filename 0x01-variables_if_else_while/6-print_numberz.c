92a64290837a7712b94e1443783fb707e88eeb11
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-03 21:39:46 +0300

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,16 @@
-#inclu
+#include <stdio.h>
+
+/**
+ * main - prints all single digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	putchar('\n');
+	return (0);
+}

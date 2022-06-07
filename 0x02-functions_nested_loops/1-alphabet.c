9928a14f5dadb02cd4a4eb2b60d4a476817daeec
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-07 22:15:08 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
-dffg
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}

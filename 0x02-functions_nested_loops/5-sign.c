5bd85254d877716f0c4d077d8583b2c984410b25
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-07 23:16:57 +0300

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,26 @@
-aisdsd
+#include "main.h"
+
+/**
+ * print_sign - gives the sign of a number
+ *
+ * @n: the parameter to be checked
+ *
+ * Return: 1 if n is greater than zero
+ * then 0 if n is zero
+ * and -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}

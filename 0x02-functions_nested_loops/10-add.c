6810e8b22c878110a8755b75b65f5887a03e6ecc
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-07 23:33:10 +0300

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,43 +1,16 @@
 #include "main.h"
 
 /**
- * print_times_table - prints tha times table of the input
- *			starting with 0.
- * @n: the value of input
+ * add - adds to numbers and output the results
+ *
+ * @a: argument to be checked
+ * @b: argument to be checked
+ * Return: 0
  */
-void print_times_table(int n)
+int add(int a, int b)
 {
-	int num, mult, prod;
+	int sum;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+	sum = a + b;
+	return (sum);
 }

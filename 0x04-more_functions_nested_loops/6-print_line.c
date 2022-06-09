8fd30879ffd338e468155f822d23effd891c9a8c
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-09 21:57:35 +0300

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,19 @@
-.ii
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of time the _ should be printed
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}

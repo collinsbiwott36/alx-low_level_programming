e0c93b7f9779920648290c361aab412cd716c2ba
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:28:52 +0300

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,32 @@
-sd
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ *
+ * @s: - string to be checked
+ *
+ * Return: the length of @s
+ */
+
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
+/**
+ * print_rev - prints a string in reverse
+ * @s: string s
+ */
+void print_rev(char *s)
+{
+	int size = _strlen(s);
+
+	while (size--)
+		_putchar(*(s + size));
+
+	_putchar('\n');
+}

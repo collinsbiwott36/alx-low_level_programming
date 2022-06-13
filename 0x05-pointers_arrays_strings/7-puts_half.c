ea29e663f6e67895f528332b61df9d53b50b7cad
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:36:01 +0300

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,26 @@
-qw
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, the last half
+ * @str: the string to be printed
+ *
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}

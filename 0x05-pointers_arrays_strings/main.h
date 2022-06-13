c0046d01b82eaaf228cb786c6774995ed5d54978
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:22:05 +0300

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,24 @@
-sa
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main header
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+size_t _strlen(const char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void put_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char*dest, const char *src);
+int _atoi(char *s);
+
+#endif

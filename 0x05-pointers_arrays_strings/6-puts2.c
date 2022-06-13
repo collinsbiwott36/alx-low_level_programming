21b31efc3fe9a56bb251e16c60098cc4011d082a
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:32:48 +0300

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
-qw
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *	strating with the first charcter
+ * @str: the string to be printed
+ */
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}

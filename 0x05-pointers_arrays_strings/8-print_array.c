2e5dfad631bbc30ac4e373cd5e3fad7d8ac4c17d
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:38:28 +0300

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,25 @@
-qw
+#include "main.h"
+
+/**
+ * print_array - prints the elements of an array
+ * @a: address to the array element
+ * @n: number of elements of the array to be printed
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}

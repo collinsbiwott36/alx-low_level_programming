4ed59acb5d767071db9ab3dd69b9d7da814bd669
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:23:29 +0300

    1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-s
+#include"main.h"
+
+/**
+ * swap_int - switches the values of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int z = *a;
+
+	*a = *b;
+	*b = z;
+}

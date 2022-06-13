530f7c205530d3e9c05e9892c4f7f3a6370693ee
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-13 23:27:32 +0300

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
-sa
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}

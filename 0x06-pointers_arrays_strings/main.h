0dc93f1af7d479505d056048a261179c0486df18
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-17 00:00:27 +0300

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,1 +1,15 @@
-disd
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header
+ *
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_stncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+
+#endif

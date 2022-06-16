090f7b6a4305fc890ff81a6996376b0547e47258
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-17 00:22:36 +0300

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_stncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_touper(char *s);
 
 #endif

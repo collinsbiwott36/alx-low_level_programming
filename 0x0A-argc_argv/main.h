7b39c01084177782fb9a5bb4415195476a1da499
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-29 11:54:09 +0300

    main

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,7 @@
-qw
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc __attribute__((unused)), char* argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int _atoi(char *s);

4e1dd1e4748d46262542dd90fd9643bbd9a1b0fd
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-29 11:53:03 +0300

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,16 @@
-qwe
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ *
+ * @argc: The number argument in the program
+ * @argv: The values of the arguments
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}

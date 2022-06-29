0f74fb8037ba6cbb44854b085b3781510e719f3d
collinsbiwott36 <48287616+collinsbiwott36@users.noreply.github.com>
2022-06-29 11:55:49 +0300

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,17 @@
-qw
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed into it
+ *
+ * @argc: the number of arguments
+ * @argv: the arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
